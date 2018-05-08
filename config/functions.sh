3a63cdf9b4df00305a3989fabc068a23d0690553
Victor Santos <vsantos.py@gmail.com>
2018-05-08 15:31:01 -0300

    add fowarding

--- config/functions.sh
+++ config/functions.sh
@@ -23,3 +23,16 @@ function graceful_terminate {
     logger 'INFO' 'Gracefully stopping tanker...'
     exit 0
 }
+
+function fowardPortHostMinikube {
+    MINIKUBE_IS_FOWARDED=$(VBoxManage showvminfo minikube | grep "host port = ${1}" || false)
+    if [[ -z "${MINIKUBE_IS_FOWARDED}" || "${MINIKUBE_IS_FOWARDED}" == '' ]]; then
+        logger 'INFO' "Fowarding minikube service port ${1} to host..."
+        VBoxManage controlvm "minikube" natpf1 "guestsvc,tcp,,${1},,${1}"
+        logger 'SUCCESS' "Finished configuration to foward port ${1} between host and minikube"
+    else
+        logger 'INFO' "Found existent NAT rule, skipping fowarding configuration..."
+    fi
+    logger 'INFO' "You may be able to access the application directly by: http://localhost:${1}"
+    logger 'INFO' "You may also be able to access Dashboard directly by: http://localhost:${MINIKUBE_DASHBOARD_PORT}"
+}
