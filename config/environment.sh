7b12c984499c74c442e1e7bd3fdfb204625f4c9e
Victor Santos <vsantos.py@gmail.com>
2018-05-23 17:29:22 -0300

    chore(kubernetes): add extra arguments to minikube exec

--- config/environment.sh
+++ config/environment.sh
@@ -4,10 +4,12 @@ DEBUG=false
 
 EXPOSE_DASHBOARD=true
 
+MINIKUBE_CLIENT_VERSION='v0.27.0'
 #MINIKUBE_DRIVER=kvm2
 MINIKUBE_DRIVER=virtualbox
 MINIKUBE_DASHBOARD_PORT=30000
 KUBERNETES_DEPLOY_NAMESPACE=development
+KUBERNETES_RBAC=true
 
 # List of k8s applications which will be deployed by tanker
 # Ex: KUBE_APPS(wordpress-sample api-java api-crm)
